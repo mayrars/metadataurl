f7dee387bc3b8ee76fcf487c81cb7180122d72fb
Mayra <mayra203@gmail.com>
2018-06-05 14:17:00 -0500

    Update errors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+	production: true,
+	firebase: {
+		apiKey: "AIzaSyD1XE98Q09O6O7FZGqFLagCWTUeh0ZoAH8",
+		authDomain: "metadata-url.firebaseapp.com",
+		databaseURL: "https://metadata-url.firebaseio.com",
+		projectId: "metadata-url",
+		storageBucket: "metadata-url.appspot.com",
+		messagingSenderId: "1028200523928"
+	}
 };
